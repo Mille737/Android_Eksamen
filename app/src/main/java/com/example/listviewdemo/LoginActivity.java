5557b37298f6337b55d5c8d4a565968f61d73e11
Holmern <48592310+Holmern@users.noreply.github.com>
2021-01-18 13:11:42 +0100

    Login Sat Op, via firebase

---
+++ app/src/main/java/com/example/listviewdemo/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.listviewdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    EditText userEmail;
+    EditText userPassword;
+    ProgressBar progressBar;
+    Button userLogin;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        progressBar = findViewById(R.id.ProgressBar);
+        userEmail = findViewById(R.id.UserEmail);
+        userPassword = findViewById(R.id.UserPassword);
+        userLogin =  findViewById(R.id.BtnUserLogin);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        userLogin.setOnClickListener(v -> mAuth.signInWithEmailAndPassword(userEmail.getText().toString(),
+                userPassword.getText().toString())
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(this, CustomListActivity.class));
+                    }else{
+                        Toast.makeText(LoginActivity.this,
+                                task.getException().getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                }));
+    }
+}
